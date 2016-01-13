7679f7eaeba4262e89997e666d961c873938e29c
Rollie Ma <rollie.ma@gmail.com>
2016-01-12 22:56:50 -0800

    Some experiment

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "golang.org/x/crypto/ssh"
 
 func main() {
-	_ = &ssh.ClientConfig{}
+	_ = &ssh.ClientConfig{
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			return nil, nil
+		},
+	}
 }
